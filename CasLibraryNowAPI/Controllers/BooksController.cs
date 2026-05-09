010b26ebb0f15eb589d72bad6234956134a0798b
STUDENT 9 <STUDENT 9@PC-8>
2026-05-09 17:31:39 +0800

    Add project files.

---
+++ CasLibraryNowAPI/Controllers/BooksController.cs
@@ -0,0 +1,121 @@
+﻿using CasLibraryNowAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Connections;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+
+namespace CasLibraryNowAPI.Controllers
+{
+    [Route("api/v1/books")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private static List<Book> books = new List<Book>
+        {
+            new Book
+            {
+                Id = 1,
+                Title = "Dune",
+                Author = "Frank Herbert",
+                Genre = "Science Fiction And Fantasy",
+                Available = true,
+                PublishedYear = 1965,
+            },
+            new Book
+            {
+                Id = 2,
+                Title = "To Kill a Mockingbird",
+                Author = "Harper Lee",
+                Genre = "Classic Fiction",
+                Available = true,
+                PublishedYear = 1960,
+            }
+        };
+
+        public IActionResult GetAll()
+        {
+            return Ok(new
+            {
+                status = "success",
+                data = books,
+                message = "Books Retrieved."
+            });
+        }
+
+        public IActionResult GetById(int id)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+                return NotFound(new
+                {
+                    status = "error",
+                    data = (object?)null,
+                    message = "Book not Found"
+                });
+            return Ok(new
+            {
+                status = "success",
+                data = book,
+                message = "Books Retrieved."
+            });
+        }
+        public IActionResult Create([FromBody] Book newBook)
+        {
+            newBook.Id = books.Count + 1;
+            books.Add(newBook);
+            return CreatedAtAction(nameof(GetById),
+                new
+                {
+                    id = newBook.Id
+                },
+                new
+                {
+                    status = "success",
+                    data = newBook,
+                    message = "Book created."
+                });
+        }
+        public IActionResult Update(int id, [FromBody] Book updateBook)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+                return NotFound(new
+                {
+                    status = "error",
+                    data = (object?)null,
+                    message = "Book not Found"
+                });
+
+            book.Title = updateBook.Title;
+            book.Author = updateBook.Author;
+            book.Genre = updateBook.Genre;
+            book.Available = updateBook.Available;
+            book.PublishedYear = updateBook.PublishedYear;
+
+            return Ok(new
+            {
+                status = "success",
+                data = book,
+                message = "Book updated."
+            });
+        }
+        public IActionResult Delete(int id)
+        {
+            var book = books.FirstOrDefault(x => x.Id == id);
+            if (book == null)
+                return NotFound(new
+                {
+                    status = "error",
+                    data = (object?)null,
+                    message = "Book not Found"
+                });
+            books.Remove(book);
+            return Ok(new
+            {
+                status = "success",
+                data = (object?)null,
+                message = "Book deleted."
+            });
+        }
+    }
+}
